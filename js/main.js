c00c570258d46df5ffea680c73030c50477aff48
Татьяна <ltasya72@gmail.com>
2023-08-17 17:35:32 +0300

    адаптирован footer

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
-$('.design__slider').slick({
+$('.desing__slider').slick({
   dots: false,
-  infinite: false,
   arrows: true,
-  speed: 300,
-  slidesToShow: 3,
+  veriableWidth: true,
+  slidesToShow: 4,
   slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
+  prevArrow: '<img class="arrow arrow-prev" src="../images/icons/arrow-prev.svg" alt="icon prev">',
+  nextArrow: '<img class="arrow arrow-next" src="../images/icons/arrow-next.svg" alt="icon prev">'
+  // responsive: [
+  //   {
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 3,
+  //       infinite: true,
+  //       dots: true
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 600,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
 
-  ]
+  // ]
 });
 
 
